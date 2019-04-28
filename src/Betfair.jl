fdefb36966a66953f91e223bed42c903af688b69
Iain Skett <iain.skett@gmail.com>
2019-04-28 22:09:02 +0100

    Current development state of quote stack

--- src/Betfair.jl
+++ src/Betfair.jl
@@ -43,6 +43,15 @@ function connect(s::Session, username::String, password::String) :: String
     s.token = body["token"]
 end
 
+const APP_SESSIONS = Dict{Symbol,Session}()
+
+function initsession()
+    settings = TOML.parsefile("cfg\\settings.toml")
+    s = Betfair.Session(settings["account"]["appid"])
+    Betfair.connect(s, settings["account"]["username"], settings["account"]["password"])
+    return s
+end
+
 function keepalive(s::Session)
     h = headers(s)
     result = HTTP.request("POST", "https://identitysso.betfair.com/api/keepAlive"; headers=h)
@@ -61,40 +70,176 @@ function call(s::Session, api::Type{T}, method::String, params::Dict{String, Any
     return body["result"]
 end
 
-getAccountFunds(s::Session) = call(s, AccountsAPI, "getAccountFunds", Dict{String,Any}())
-getAccountDetails(s::Session) = call(s, AccountsAPI, "getAccountDetails", Dict{String,Any}())
-getAccountStatement(s::Session) = call(s, AccountsAPI, "getAccountStatement", Dict{String,Any}())
+abstract type Event end
+abstract type AbstractMarket{E <: Event} end
+
+struct Runner{E <: Event, M <: AbstractMarket{E}}
+    name::String
+    id::Int64
+    market::M
+end
+
+mutable struct Market{E <: Event} <: AbstractMarket{E}
+    name::String
+    id::String
+    event::E
+    _runners::Dict{String,Runner{E, Market{E}}}
+
+    Market(name::String, id::String, event::E) where {E <: Event} = new{E}(name, id, event, Dict{String,Runner{E, Market{E}}}())
+end
+
+mutable struct FootballEvent <: Event
+    name::String
+    id::String
+    _markets::Dict{String,Market{FootballEvent}}
+
+    FootballEvent(name::String, id::String) = new(name, id, Dict{String,Market{FootballEvent}}())
+end
+
+@enum Side begin
+    Back = 1
+    Lay = -1
+end
+
+struct Quote
+    side::Side
+    price::Float64
+    size::Float64
+end
+
+struct QuoteStack
+    backdepth::Vector{Quote}
+    laydepth::Vector{Quote}
+end
+
+bestback(qs::QuoteStack) :: Union{Nothing,Quote} = length(qs.backdepth) > 0 ? qs.backdepth[1] : nothing
+bestlay(qs::QuoteStack) :: Union{Nothing,Quote} = length(qs.laydepth) > 0 ? qs.laydepth[1] : nothing
+
+fillablequote(qs::QuoteStack, side::Side, size::Float64; )
+
+@enum FillMethod begin
+    Average = 1
+    Full = 2
+end
+
+function fillablebackquote(qs::QuoteStack, size::Float64; method::FillMethod = Average) :: Tuple{Quote, Bool}
+    quotesize = 0.0
+    totalprice = 0.0
+
+    for qt in qs.backdepth
+        Match.@match Int(method) begin
+            Int(Average)         => (fill = min(qt.size,size - quotesize); quotesize += fill; totalprice += fill * qt.price)
+            Int(Full)            => quotesize == 0.0 && qt.size >= size ? (totalprice = qt.price * qt.size; quotesize = qt.size) : nothing
+            _                    => error("Unknown method $(method)")
+        end
+    end
+    return (Quote(Back, totalprice / quotesize, quotesize), quotesize == size)
+end
+
+function fillablelayquote(qs::QuoteStack, size::Float64; method::FillMethod = Average) :: Tuple{Quote, Bool}
+    quotesize = 0.0
+    totalprice = 0.0
+    size - quotesize
+
+    for qt in qs.laydepth
+        Match.@match method begin
+            Average         => (fill = min(qt.size,size - quotesize); quotesize += fill; totalprice += fill * qt.price)
+            Full            => quotesize == 0.0 && qt.size >= size ? (totalprice = qt.price * qt.size; quotesize = qt.size) : nothing
+        end
+    end
+    return (Quote(Back, quoteprice / quotesize, quotesize), quotesize == size)
+end
+
+
+pricetoback(qs::QuoteStack, size::Float64)
+pricetolay()
+
+function inplaytimeline(s::Session, e::FootballEvent)
+    res = HTTP.request("GET", "https://ips.betfair.com/inplayservice/v1/eventTimeline?alt=json&eventId=$(e.id)&locale=en_GB&productType=EXCHANGE&regionCode=UK")
+    body = JSON.Parser.parse(String(res.body))
+    length(body) != 0 || return Dict{String,Any}("status" => "NOT_IN_PLAY")
+    # Reduce it to data we're interested in at the moment.
+    return Dict{String,Any}(
+        "elapsedtime" => body["timeElapsed"],
+        "status" => body["status"],
+        "elapsedregulartime" => body["elapsedRegularTime"],
+        "score" => (parse(Int8, body["score"]["home"]["score"]), parse(Int8,body["score"]["away"]["score"]))
+    )
+end
+
+function markets(event::Event, session::Session; refresh::Bool = false)
+    if length(event._markets) == 0 || refresh
+        params = Dict{String,Any}("filter" => Dict{String,Any}("eventIds" => [event.id]), "maxResults" => 1000)
+        results = call(session, BettingAPI, "listMarketCatalogue", params)
+        for result in results
+            market = Market(result["marketName"], result["marketId"], event)
+            event._markets[market.name] = market
+        end
+    end
+    return event._markets
+end
+
+function runners(market::Market{E}, session::Session; refresh::Bool = false) where {E <: Event}
+    if length(market._runners) == 0 || refresh
+        params = Dict{String,Any}("filter" => Dict{String,Any}("marketIds" => [market.id]), "maxResults" => 1000, "marketProjection" => ["RUNNER_DESCRIPTION"])
+        results = call(session, BettingAPI, "listMarketCatalogue", params)
+        length(results) == 1 || error("Expect only one entry in market catalogue for $(market.name), found $(length(results))")
+        for entry in results[1]["runners"]
+            runner = Runner{E, typeof(market)}(entry["runnerName"], entry["selectionId"], market)
+            market._runners[runner.name] = runner
+        end
+    end
+
+    return market._runners
+end
+
+function marketdata(market::Market{E}, session::Session) where {E <: Event}
+    params = Dict{String,Any}("marketIds" => [market.id], "priceProjection" => Dict{String,Any}("priceData" => ["EX_ALL_OFFERS"]))
+    results = call(session, BettingAPI, "listMarketBook", params)
+    length(results) == 1 || error("Expected only one entry in market book for $(market.name), found $(length(results))")
+    resbyrunner = reduce((x,y) -> (x[y["selectionId"]] = y; x), results[1]["runners"]; init=Dict{Int64,Any}())
+    ret = Dict{Runner{E, Market{E}}, QuoteStack}()
+    for (name,runner) in runners(market, session)
+        offers = resbyrunner[runner.id]["ex"]
+        ret[runner] = QuoteStack(map(x -> Quote(Back, x["price"], x["size"]), offers["availableToBack"]), map(x -> Quote(Lay, x["price"], x["size"]), offers["availableToLay"]))
+    end
+    return ret
+end
+
+getaccountfunds(s::Session) = call(s, AccountsAPI, "getAccountFunds", Dict{String,Any}())
+getaccountdetails(s::Session) = call(s, AccountsAPI, "getAccountDetails", Dict{String,Any}())
+getaccountstatement(s::Session) = call(s, AccountsAPI, "getAccountStatement", Dict{String,Any}())
 
 # Some specific overrides to standard terminology for
-const eventTypeOverrides = Dict{String, String}("Football" => "Soccer")
+const EVENT_TYPE_OVERRIDES = Dict{String, String}("Football" => "Soccer")
 
-Memoize.@memoize function listEventTypes(s::Session)
+Memoize.@memoize function listeventtypes(s::Session)
     res = call(s, BettingAPI, "listEventTypes", Dict{String,Any}("filter" => Dict{String,Any}()))
     types = Dict(map(x->x["eventType"]["name"], res) .=> map(x->x["eventType"]["id"], res))
-    for (k,v) in eventTypeOverrides
+    for (k,v) in EVENT_TYPE_OVERRIDES
         types[k] = types[v]
     end
 
     return types
 end
 
-function extendedEventType(event)
+function extendevent(event)
     return Dict("openDateTime" => Dates.parse(Dates.DateTime, event["openDate"], Dates.DateFormat("yyyy-mm-dd\\THH:MM:SS.sZ")))
 end
 
-function listEvents(s::Session, event::String)
-    eventtypes = listEventTypes(s)
+function listevents(s::Session, event::String)
+    eventtypes = listeventtypes(s)
     res = call(s, BettingAPI, "listEvents", Dict{String,Any}("filter" => Dict{String,Any}("eventTypeIds" => [eventtypes[event]])))
-    events = Dict(map(x->x["event"]["id"], res) .=> map(x->merge(extendedEventType(x["event"]),x["event"]), res))
+    events = Dict(map(x->x["event"]["id"], res) .=> map(x->merge(extendevent(x["event"]),x["event"]), res))
     return events
 end
 
-function listMarketCatalogue(s::Session, event::String)
+function getmarketcatalogue(s::Session, event::String)
     params = Dict{String,Any}("filter" => Dict{String,Any}("eventIds" => [event]), "maxResults" => 1000)
     return call(s, BettingAPI, "listMarketCatalogue", params)
 end
 
-function getEventMarkets(s::Session, eventId::String; pivot::Symbol = :name)
+function geteventmarkets(s::Session, eventId::String; pivot::Symbol = :name)
     fields = ["MARKET_DESCRIPTION"]
     params = Dict{String,Any}("filter" => Dict{String,Any}("eventIds" => [eventId]), "maxResults" => 1000, "marketProjection" => fields)
     res = call(s, BettingAPI, "listMarketCatalogue", params)
@@ -106,7 +251,7 @@ function getEventMarkets(s::Session, eventId::String; pivot::Symbol = :name)
     return pivotres
 end
 
-function getMarketRunners(s::Session, marketId::String, pivot::Symbol = :name)
+function getmarketrunners(s::Session, marketId::String, pivot::Symbol = :name)
     fields = ["RUNNER_DESCRIPTION"]
     params = Dict{String,Any}("filter" => Dict{String,Any}("marketIds" => [marketId]), "maxResults" => 1000, "marketProjection" => fields)
     res = call(s, BettingAPI, "listMarketCatalogue", params)
@@ -119,19 +264,19 @@ function getMarketRunners(s::Session, marketId::String, pivot::Symbol = :name)
     return pivotrunners
 end
 
-function getMarketBook(s::Session, marketId::String)
+function getmarketbook(s::Session, marketId::String)
     params = Dict{String,Any}("marketIds" => [marketId], "priceProjection" => Dict{String,Any}("priceData" => ["EX_ALL_OFFERS"]), "orderProjection" => "EXECUTABLE")
     res = call(s, BettingAPI, "listMarketBook", params)
     return res
 end
 
-function getRunnerBook(s::Session, marketId::String, runnerId::Int64)
+function getrunnerbook(s::Session, marketId::String, runnerId::Int64)
     params = Dict{String,Any}("marketId" => marketId, "selectionId" => runnerId)
     res = call(s, BettingAPI, "listRunnerBook", params)
     return res
 end
 
-function listCurrentOrders(s::Session; requestinc = 1000)
+function listcurrentorders(s::Session; requestinc = 1000)
     res = call(s, BettingAPI, "listCurrentOrders", Dict{String,Any}("recordCount" => requestinc))
     orders = res["currentOrders"]; fromidx = requestinc;
     while res["moreAvailable"]
@@ -140,13 +285,6 @@ function listCurrentOrders(s::Session; requestinc = 1000)
         append!(orders, res["currentOrders"])
     end
     return orders
-end
-
-function initsession()
-    settings = TOML.parsefile("cfg\\settings.toml")
-    s = Betfair.Session(settings["account"]["appid"])
-    Betfair.connect(s, settings["account"]["username"], settings["account"]["password"])
-    return s
 end
 
 end # module
