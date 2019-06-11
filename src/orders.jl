6059ef9a08f9f40bad3d86bf6c2d8c522abda16c
Iain Skett <iain.skett@gmail.com>
2019-06-11 08:14:28 +0100

    Add support for caching of bets within the session and refreshing them

--- src/orders.jl
+++ src/orders.jl
@@ -3,13 +3,10 @@
     ExecutionComplete = 2
     Executable = 3
     Expired = 4
+    Cancelled = 5
 end
 
-struct OrderKey <: Key
-    id::String
-end
-
-struct OrderState
+mutable struct OrderState
     status::OrderStatus
     id::Union{Nothing,OrderKey}
     sizematched::Float64
@@ -25,12 +22,11 @@ const OrderStatusMap = Dict(zip(map(Symbol∘lowercase∘String∘Symbol, instan
 orderstatus(key::Symbol) = OrderStatusMap[key]
 orderstatus(key::String) = orderstatus(Symbol(lowercase(key)))
 
-abstract type Order end
 marketkey(o::Order) = error("marketkey not implemented by $(typeof(o))")
 instruction(o::Order) = error("instruction not implemented by $(typeof(o))")
 state(o::Order) = error("state not implemented by $(typeof(o))")
 
-mutable struct LimitOrder <: Order
+struct LimitOrder <: Order
     marketkey::MarketKey
     runnerkey::RunnerKey
     side::Side
@@ -92,6 +88,9 @@ function instruction(o::FillOrKillOrder)
 end
 
 function place(s::Session, order::O) where {O<:Order}
+    state = Betfair.state(order)
+    state.id === nothing || error("Order $(state.id.id) has already been placed")
+
     params = Dict(
         "marketId" => marketkey(order).id,
         "instructions" => [instruction(order)]
@@ -100,21 +99,66 @@ function place(s::Session, order::O) where {O<:Order}
     res["status"] == "SUCCESS" || error("failed to place order $(o) with status $(res["status"])")
     length(res["instructionReports"]) == 1 || error("Unexpected number of instruction reports returned")
     rpt = res["instructionReports"][1]
-    order.state = OrderState(
-        orderstatus(rpt["orderStatus"]),
-        OrderKey(rpt["betId"]),
-        rpt["sizeMatched"],
-        rpt["averagePriceMatched"],
-        Dates.parse(Dates.DateTime, rpt["placedDate"], Dates.DateFormat("yyyy-mm-dd\\THH:MM:SS.sZ"))
+
+    state.status = orderstatus(rpt["orderStatus"])
+    state.id = OrderKey(rpt["betId"])
+    state.sizematched = rpt["sizeMatched"]
+    state.pricematched = rpt["averagePriceMatched"]
+    state.placedat = Dates.parse(Dates.DateTime, rpt["placedDate"], Dates.DateFormat("yyyy-mm-dd\\THH:MM:SS.sZ"))
+
+    s.orders[state.id] = order
+    order
+end
+
+refreshorders(s::Session, market::Market) = refreshorders(s, market.key)
+function refreshorders(s::Session, key::MarketKey)
+    params = Dict(
+        "marketIds" => [key.id],
+        "orderProjection" => "ALL",
+        "fromRecord" => 0,
+        "recordCount" => 1000
     )
+    moreavailable = true
+    orders = []
+    while moreavailable
+        res = call(s, BettingAPI, "listCurrentOrders", params)
+        params["fromRecord"] += 1000
+        moreavailable = res["moreAvailable"]
+        for defn in res["currentOrders"]
+            order = get!(s.orders, OrderKey(defn["betId"])) do
+                order = LimitOrder(
+                    MarketKey(defn["marketId"]),
+                    RunnerKey(defn["selectionId"]),
+                    if defn["side"] == "BACK" Back else Lay end,
+                    defn["priceSize"]["size"],
+                    defn["priceSize"]["price"]
+                )
+                Betfair.state(order).id = OrderKey(defn["betId"])
+            end
+
+            state = Betfair.state(order)
+            state.status = orderstatus(defn["status"])
+            state.sizematched = defn["sizeMatched"]
+            state.pricematched = defn["averagePriceMatched"]
+            state.placedat = Dates.parse(Dates.DateTime, defn["placedDate"], Dates.DateFormat("yyyy-mm-dd\\THH:MM:SS.sZ"))
+
+            push!(orders, order)
+        end
+    end
+
+    orders
 end
 
 function cancel(s::Session, o::O) where {O<:Order}
-    state = state(o)
+    state = Betfair.state(o)
     state.status == Executable || error("Can only cancel an executable order")
 
     params = Dict(
+        "marketId" => marketkey(o).id,
         "instructions" => [Dict("betId" => state.id.id)]
     )
     res = call(s, BettingAPI, "cancelOrders", params)
+    # TODO: Check that we've actually succeeded and that we've cancelled all outstanding
+    state.status = Cancelled
+    o
 end
